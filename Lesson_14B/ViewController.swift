59cee510cb15fe818adf1ec98bb8a273bd289170
Данила Дудкин <danildudkin@yandex.ru>
2021-01-29 13:44:14 +0300

    Initial Commit

---
+++ Lesson_14B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_14B
+//
+//  Created by Danila Dudkin on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
